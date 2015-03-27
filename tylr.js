f4ac616519a4ebf6b49be246b13d361257e49663
christophe-marteau <christophe-marteau@users.noreply.github.com>
2015-03-27 14:47:37 +0100

    Add a new method to select tile

--- tylr.js
+++ tylr.js
@@ -1,27 +1,24 @@
-var nfs = require('node-fs'),
-  async = require('async');
+var nfs = require('node-fs');
+var async = require('async');
 
 module.exports = {
 
-  tyle: function( file, dir, levels ){
+  tyle: function( file, dir, levels, algo ){
     var self = this;
 
+    console.log( file, dir, levels, algo );
+
     if ( nfs.existsSync( file ) ) {
-
       nfs.readFile( file, function( err, data ){
+        console.log('data', data)
         var features = JSON.parse(data).features;
-
+  
         features.forEach(function( f ){
-          //var ll = self.center( f.geometry );
-          var ll = self.centroid(f.geometry);
-          //console.log( ll );
-
-          var z = parseInt( levels[0] );
-
-          while ( z <= levels[ 1 ] ) {
-            var xyz = self.location( ll[1], ll[0], z )
-            self.q.push({ dir: dir, x: xyz.x, y: xyz.y, z: xyz.z, feature: f }, function (err) {} );
-            z++;
+          var llArray = [];
+          llArray = self.getTiles(f.geometry , levels, algo);
+          for(i = 0 ;i < llArray.length; i++) {
+            console.log( llArray[i] );
+            self.q.push({ dir: dir, x: llArray[i].x, y: llArray[i].y, z: llArray[i].z, feature: f }, function (err) {} );
           }
         });
       });
@@ -31,73 +28,10 @@ module.exports = {
 
   },
 
-  centroid: function (geom) {
-    // http://en.wikipedia.org/wiki/Centroid#Centroid_of_polygon
 
-    function signedArea (coords) {
-      var sum = 0.0;
+  getTiles: function(geom, levels, algo ){
+    var self = this;
 
-      for (var i = 0, len = coords.length - 1; i < len; i++) {
-        var p1 = coords[i],
-            p2 = coords[i + 1];
-
-        sum += (p1[0] * p2[1]) - (p1[1] * p2[0]);
-      }
-
-      return 0.5 * sum;
-    }
-
-    function getCentroid (coords) {
-      var area = signedArea(coords);
-
-      if (area == 0) {
-        console.log('Error: Invalid coordinates ' + coords);
-        return false;
-        //throw new Error('Invalid coordinates: ' + coords);
-      }
-
-      var p1, p2, f;
-      var x = 0.0,
-          y = 0.0;
-
-      for (var i = 0, len = coords.length - 1; i < len; i++) {
-        p1 = coords[i];
-        p2 = coords[i+1];
-
-        x += (p1[0] + p2[0]) * ((p1[0] * p2[1]) - (p2[0] * p1[1]));
-        y += (p1[1] + p2[1]) * ((p1[0] * p2[1]) - (p2[0] * p1[1]));
-      }
-
-      f = area * 6;
-
-      return [x/f, y/f];
-    }
-
-    if (geom.type == 'MultiPolygon') {
-      var centroids = [];
-
-      geom.coordinates[0].forEach(function (polygon) {
-        centroids.push(getCentroid(polygon));
-      });
-
-      if (centroids.length >= 3) {
-        centroids.push(centroids[0]);
-        return getCentroid(centroids);
-      } else if (centroids.length == 2) {
-        if (centroids[0] == centroids[1]) {
-          return centroids[0];
-        } else {
-          return [(centroids[0][0] + centroids[1][0]) / 2, (centroids[0][1] + centroids[1][1]) / 2];
-        }
-      } else if (centroids.length == 1) {
-        return centroids[0];
-      }
-    } else {
-      return getCentroid(geom.coordinates[0]);
-    }
-  },
-
-  center: function( geom ){
     var minx, miny, maxx, maxy;
 
     function adjust( c ){
@@ -126,27 +60,54 @@ module.exports = {
         miny = coords[0][1],
         maxx = coords[0][0],
         maxy = coords[0][1];
-
-      adjust( geom.coordinates[0] );
+      
+      adjust( geom.coordinates[0] ); 
     }
-    return [ (maxx + minx) / 2, (maxy + miny) / 2 ];
+    var z = parseInt( levels[0] );
+    var xyzArray = [];
+  
+    while ( z <= levels[ 1 ] ) {
+      switch( algo ) {
+        case 'center':
+          // We add one tile touching the center of the boundary box of the geo area
+          xyzArray.push( self.location( (maxy + miny) / 2, (maxx + minx) / 2, z ) );
+          break;
+        case 'box':
+          // We add all the tiles touching the boundary box of the geo area
+          // * Taking the tile up right and the tile left down
+          var xyzMin = self.location( miny , minx, z );
+          var xyzMax = self.location( maxy , maxx, z );
+          console.log('box : x [' + xyzMin.x + ', ' + xyzMax.x + '] - y [' + xyzMax.y + ', ' + xyzMin.y + ']'); 
+          // for all tile in this area
+          for( y = xyzMax.y; y <= xyzMin.y; y++) {
+            for( x = xyzMin.x; x <= xyzMax.x; x++) {
+              xyzArray.push( {y: y , x: x, z: z } );
+            }
+          }
+          break;
+        default:
+          console.log('Algo "' + algo + '" undefined. Available : "center" or box' );
+      }
+      z++;
+    }
+    return( xyzArray );
   },
 
   location: function( lat, lon, zoom ) {
     var lon_rad = lon * (Math.PI / 180),
       lat_rad = lat * (Math.PI / 180),
       n = Math.pow(2.0, zoom);
-
+  
     var tileX = Math.floor( ( ( lon + 180 ) / 360 ) * n );
     var tileY = Math.floor( ( 1 - ( Math.log( Math.tan( lat_rad ) + 1.0 / Math.cos( lat_rad ) ) / Math.PI ) ) * n / 2.0 );
-
+  
     return { x : tileX, y: tileY, z: zoom };
   },
 
   q: async.queue(function (task, callback) {
 
     var p = [task.dir, task.z, task.x].join('/');
-    var file = p + '/' + task.y + '.json';
+    var file = p + '/' + task.y + '.png';
 
     nfs.mkdir( p, '0777', true, function(){
       if ( !nfs.existsSync( file ) ) {
@@ -167,6 +128,7 @@ module.exports = {
 
 
 };
+  
 
 /*function centroid( coords ){
   var area = 0;
